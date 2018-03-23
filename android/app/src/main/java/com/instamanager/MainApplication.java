97209c071c1afdd35f83104180e727ed34d10f6c
davidguillemet <davidguillemet@iMac-27-DGUI.local>
2018-03-23 21:31:46 +0100

    Adding Facebook SDK

--- android/app/src/main/java/com/instamanager/MainApplication.java
+++ android/app/src/main/java/com/instamanager/MainApplication.java
@@ -3,6 +3,7 @@ package com.instamanager;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.realm.react.RealmReactPackage;
 import com.psykar.cookiemanager.CookieManagerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
             new VectorIconsPackage(),
             new RealmReactPackage(),
             new CookieManagerPackage()
