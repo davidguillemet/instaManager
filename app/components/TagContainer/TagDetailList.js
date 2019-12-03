73e72dc4bfca4d3120fda0bf6105c1933f6192c5
David Guillemet <david.guillemet@hotmail.com>
2019-12-03 22:32:19 +0100

    TagDetailList not updated when a tag is removed

--- app/components/TagContainer/TagDetailList.js
+++ app/components/TagContainer/TagDetailList.js
@@ -22,8 +22,7 @@ export default class TagDetailList extends React.PureComponent {
         this.state = {
             sortedColumn: 0,
             sortOrder: [ SORT_ORDER.ASCENDING, SORT_ORDER.ASCENDING ],
-            isSwiping: false,
-            tags: this.props.tags
+            isSwiping: false
         }
         this.renderMediaCountItem = this.renderMediaCountItem.bind(this);
         this.renderListItem = this.renderListItem.bind(this);
@@ -73,7 +72,7 @@ export default class TagDetailList extends React.PureComponent {
     }
 
     onTransientMediaCountUpdated() {
-        this.setState({ tags: [ ...this.state.tags ] });
+        this.setState({ state: this.state });
     }
 
     toggleSortFirstColumn() {
@@ -87,7 +86,7 @@ export default class TagDetailList extends React.PureComponent {
     sortTags() {
         
         const sortOrder = this.state.sortOrder[this.state.sortedColumn];        
-        this.state.tags.sort((tag1, tag2) => {
+        this.props.tags.sort((tag1, tag2) => {
             const t1 = sortOrder == SORT_ORDER.ASCENDING ? tag1 : tag2;
             const t2 = sortOrder == SORT_ORDER.ASCENDING ? tag2 : tag1;
             switch (this.state.sortedColumn) {
@@ -143,7 +142,7 @@ export default class TagDetailList extends React.PureComponent {
 
     renderListHeader() {
         
-        const sortDisabled = this.state.tags.length <= 1;
+        const sortDisabled = this.props.tags.length <= 1;
 
         const headerStyle = {
             flex: 1,
@@ -166,7 +165,7 @@ export default class TagDetailList extends React.PureComponent {
         }
 
         // We cannot sort on media count if at least one item is refreshing
-        const sortOnMediaCountDisabled = (this.state.tags.findIndex(t => t.mediaCount == null) >= 0);
+        const sortOnMediaCountDisabled = (this.props.tags.findIndex(t => t.mediaCount == null) >= 0);
         if (sortOnMediaCountDisabled) {
             mediaCountHeaderTextStyle.color = CommonStyles.DEACTIVATED_TEXT_COLOR;
         }
@@ -206,7 +205,7 @@ export default class TagDetailList extends React.PureComponent {
 
         return (
             <FlatList
-                data={this.state.tags}
+                data={this.props.tags}
                 extraData={this.state}
                 keyExtractor={this.mediaCountKeyExtractor}
                 renderItem={this.renderMediaCountItem}
